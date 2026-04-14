6c2634bc0567323bfaecaa02dd4339f718d064ba
aihhhhhh521 <aihjyhyb@outlook.com>
2026-04-14 17:33:53 +0800

    fast_llama

--- Uni-NaVid/uninavid/model/language_model/fastv_llama.py
+++ Uni-NaVid/uninavid/model/language_model/fastv_llama.py
@@ -1,8 +1,19 @@
 from typing import List, Optional, Tuple, Union
 
+import os
 import torch
+import torch.nn as nn
+from torch.nn import CrossEntropyLoss
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    LlamaConfig,
+    LlamaForCausalLM,
+)
 from transformers.models.llama.modeling_llama import LlamaModel
-from transformers.modeling_outputs import BaseModelOutputWithPast
+from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
+
+from uninavid.model.uninavid_arch import UniNaVIDMetaModel, UniNaVIDMetaForCausalLM
 
 try:
     from transformers.models.llama.modeling_llama import _prepare_4d_causal_attention_mask
@@ -229,3 +240,127 @@ class FastVLlamaModel(LlamaModel):
                 expanded = (1.0 - attention_mask[:, None, None, :].to(dtype)) * torch.finfo(dtype).min
                 causal = causal + expanded
         return causal
+
+
+print("Setting WANDB_MODE to offline")
+os.environ["WANDB_MODE"] = "offline"
+
+
+class LlavaFastVConfig(LlamaConfig):
+    model_type = "llava_fastv"
+
+
+class LlavaAttFastVLlamaModel(UniNaVIDMetaModel, FastVLlamaModel):
+    config_class = LlavaFastVConfig
+
+    def __init__(self, config: LlamaConfig):
+        super(LlavaAttFastVLlamaModel, self).__init__(config)
+
+
+class LlavaLlamaAttFastVForCausalLM(LlamaForCausalLM, UniNaVIDMetaForCausalLM):
+    config_class = LlavaFastVConfig
+
+    def __init__(self, config):
+        super(LlamaForCausalLM, self).__init__(config)
+        self.model = LlavaAttFastVLlamaModel(config)
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.post_init()
+
+    def get_model(self):
+        return self.model
+
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        images: Optional[torch.FloatTensor] = None,
+        prompts: Optional[List[str]] = None,
+        return_dict: Optional[bool] = None,
+        fastv_config: Optional[dict] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if not self.training:
+            if images[0].device != self.device:
+                images[0] = images[0].to(device=self.device)
+            if input_ids.device != self.device:
+                input_ids = input_ids.to(device=self.device)
+
+        input_ids, attention_mask, past_key_values, inputs_embeds, labels = self.prepare_inputs_labels_for_multimodal(
+            input_ids, attention_mask, past_key_values, labels, images, prompts=prompts
+        )
+
+        torch.cuda.empty_cache()
+
+        outputs = self.model.fastv_forward(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            fastv_config=fastv_config,
+        )
+
+        hidden_states = outputs[0]
+        logits = self.lm_head(hidden_states)
+
+        loss = None
+        if labels is not None:
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            loss_fct = CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+    def prepare_inputs_for_generation(
+        self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
+    ):
+        if past_key_values:
+            input_ids = input_ids[:, -1:]
+
+        if inputs_embeds is not None and past_key_values is None:
+            model_inputs = {"inputs_embeds": inputs_embeds}
+        else:
+            model_inputs = {"input_ids": input_ids}
+
+        model_inputs.update(
+            {
+                "past_key_values": past_key_values,
+                "use_cache": kwargs.get("use_cache"),
+                "attention_mask": attention_mask,
+                "images": kwargs.get("images", None),
+                "fastv_config": kwargs.get("fastv_config", None),
+            }
+        )
+        return model_inputs
+
+
+AutoConfig.register("llava_fastv", LlavaFastVConfig)
+AutoModelForCausalLM.register(LlavaFastVConfig, LlavaLlamaAttFastVForCausalLM)
